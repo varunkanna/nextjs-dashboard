4ae8a9b095068973e2ccd757c5f86ceef82d16b4
Gangeswaran K <96485946+varunkanna@users.noreply.github.com>
2026-01-02 04:36:23 +0530

    initial Setup

---
+++ app/ui/font.ts
@@ -0,0 +1,10 @@
+import { Inter } from 'next/font/google';
+
+export const inter = Inter({
+    subsets: ['latin'],
+})
+
+export const Lusitana = Inter({
+    weight: ['400', '700'],
+    subsets: ['latin'],
+})
